8f9cdef3a57c433af96d41be0337b49ea90510f6
seforarodriguez <seforarodriguez13@gmail.com>
2016-01-14 20:33:00 -0600

    deleted a commented code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -64,14 +64,6 @@ angular.module('starter.controllers', ["noteContentFactory"])
 
   $scope.noteListObject = notes;
 
-  // $scope.noteListObject = [
-  //   { title: 'Reggae', id: 1 },
-  //   { title: 'Chill', id: 2 },
-  //   { title: 'Dubstep', id: 3 },
-  //   { title: 'Indie', id: 4 },
-  //   { title: 'Rap', id: 5 },
-  //   { title: 'Cowbell', id: 6 }
-  // ];
 })
 
 .controller('noteCtrl', function($scope, $stateParams, noteContentFactory) {
